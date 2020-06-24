5ca185cc9ae0c297d867208a58b81bfd6dbe6a21
paizsvirag <paizs.virag@gmail.com>
2020-06-24 23:53:25 +0200

    backend first phase done

---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require('fs'); 
+const path = require('path');
+const csv = require('csv-parser');
+const express = require('express')
+const app = express()
+const port = 3000
+
+const allData = {}
+
+fs.readdirSync('./db').forEach(file => {
+    const fileName = path.basename(file, '.csv')
+    const filePath = path.resolve(__dirname, 'db', file);
+
+    if(path.extname(file) !== '.csv') {
+        return
+    }
+
+    fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (data) => {
+            if (Object.values(data).every(value => value === '')) {
+                return
+            }
+            if (!(fileName in allData)) {
+                allData[fileName] = []
+            }
+            allData[fileName].push(data);
+        })
+        .on('end', () => {
+            console.log(`${fileName} was successfully processed`);
+        });
+});
+
+app.get('/', (req, res) => res.send(`Hello World!`))
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
