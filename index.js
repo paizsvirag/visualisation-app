b75fc7bdb68dc6ebb7c99edecf0456ea69977b98
paizsvirag <paizs.virag@gmail.com>
2020-06-28 23:22:11 +0200

    added front end barebone and connected it with backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const csv = require('csv-parser');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
 const allData = {}
 
@@ -31,6 +31,6 @@ fs.readdirSync('./db').forEach(file => {
         });
 });
 
-app.get('/', (req, res) => res.send(`Hello World!`))
+app.get('/api/data', (req, res) => res.send(`Hello World!`))
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
